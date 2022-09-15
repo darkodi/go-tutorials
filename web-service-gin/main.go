6af7b1e958aa128d840a0512d82aa209630e9b26
darko.deuric <darko.deuric@ethernal.tech>
2022-09-15 20:58:33 +0200

    add router and resolve GET http call to albums endpoint

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -21,7 +21,16 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+func main() {
+	router := gin.Default() // Initialize a Gin router using Default.
+	// this sets up an association in which getAlbums handles requests to the /albums endpoint path.
+	// in other words, use the GET function to associate the GET HTTP method and /albums path with a handler function.
+	router.GET("/albums", getAlbums)
+	// attach the router to an http.Server and start the server.
+	router.Run("localhost:8080")
+}
+
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	c.IndentedJSON(http.StatusOK, albums) //  serialize the struct into JSON and add it to the response.
 }

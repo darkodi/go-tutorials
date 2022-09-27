0bf01d4ed7a2404a392c53cb5dc83c45d4eb9a61
darko.deuric <darko.deuric@ethernal.tech>
2022-09-27 20:44:52 +0200

    simple web server

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -24,9 +26,16 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+//extracts the page title from r.URL.Path, the path component of the request URL
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// The Path is re-sliced with [len("/view/"):] to drop the leading "/view/" component of the request path
+	//This is because the path will invariably begin with "/view/", which is not part of the page's title.
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
